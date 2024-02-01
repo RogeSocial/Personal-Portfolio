c85161472374269295bc2ec14b9f0057c0c4e4f3
Linus Rengbrandt <line.rengbrandt@gmail.com>
2024-02-01 16:52:31 +0100

    navbar

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -1,7 +1,12 @@
 function navbar () {
     return (
-        <div>
-            <h1>Linus Rengbrandt</h1>
+        <div className="navbar">
+            <h1 className="nav-title">Linus Rengbrandt</h1>
+            <div className="nav-right">
+                <a href="/about">About</a>
+                <a href="/projects">Projects</a>
+                <a href="/contact">Contact</a>
+            </div>
         </div>
     )
 }
