ac294460649140f4d3a89df8c0fbc322605152ff
Linus Rengbrandt <line.rengbrandt@gmail.com>
2024-02-01 23:09:41 +0100

    home container done

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,28 +1,46 @@
-function LandingPage () {
-    return (
-        <>
-            <div className="home-container">
-                <h1>I'm Linus, a <br></br> Fullstack Developer</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident vel dolorum exercitationem in quaerat nemo, 
-                cupiditate rem animi voluptatum illo ea atque alias molestias ullam esse soluta delectus vitae eius.</p>
-            </div>
-            
-            <div className="about-container">
-                <h1>About Me</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident vel dolorum exercitationem in quaerat nemo</p>
-            </div>
+import "../styling/home-con.css";
 
-            <div className="projects-container">
-                <h1>Projects</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident vel dolorum exercitationem in quaerat nemo</p>
-            </div>
+function LandingPage() {
+  return (
+    <>
+      <div className="home-container">
+        <h1>
+          Hello, I'm <span className="name-highlight">Linus Rengbrandt</span>
+          <br></br> I'm a Fullstack Developer
+        </h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident
+          vel dolorum exercitationem in quaerat nemo, cupiditate rem animi
+          voluptatum illo ea atque alias molestias ullam esse soluta delectus
+          vitae eius.
+        </p>
+      </div>
 
-            <div className="contact-container">
-                <h1>Contact</h1>
-                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident vel dolorum exercitationem in quaerat nemo</p>
-            </div>
-        </>
-    )
+      <div className="about-container">
+        <h1>About Me</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident
+          vel dolorum exercitationem in quaerat nemo
+        </p>
+      </div>
+
+      <div className="projects-container">
+        <h1>Projects</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident
+          vel dolorum exercitationem in quaerat nemo
+        </p>
+      </div>
+
+      <div className="contact-container">
+        <h1>Contact</h1>
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident
+          vel dolorum exercitationem in quaerat nemo
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default LandingPage;
