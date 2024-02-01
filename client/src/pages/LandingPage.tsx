5d89d6797999c015ab2878eccc0fcd888678150c
Linus Rengbrandt <line.rengbrandt@gmail.com>
2024-02-02 00:04:59 +0100

    added styling to the about me container

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,4 +1,6 @@
 import "../styling/home-con.css";
+import "../styling/about-con.css";
+import { Link } from "react-router-dom";
 
 function LandingPage() {
   return (
@@ -17,11 +19,24 @@ function LandingPage() {
       </div>
 
       <div className="about-container">
-        <h1>About Me</h1>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident
-          vel dolorum exercitationem in quaerat nemo
+        <h1>
+          <span className="highlight">/</span> About Me
+        </h1>
+        <h2>
+          Passionate Full-Stack Developer: <br></br>
+          Crafting Digital Excellence with Every Line of Code.
+        </h2>
+        <p className="about-info">
+          Ever since I was a child, my fascination with computers and
+          programming has been a guiding force. Now, as I navigate the exciting
+          journey of becoming a Full-Stack Developer, my childhood curiosity has
+          transformed into a relentless pursuit of knowledge. Each line of code
+          is a step towards turning passion into expertise, as I strive to bring
+          my long-held interest to life in the dynamic world of programming.
         </p>
+        <Link to="/" className="more-about">
+          <p>More about me</p>
+        </Link>
       </div>
 
       <div className="projects-container">
