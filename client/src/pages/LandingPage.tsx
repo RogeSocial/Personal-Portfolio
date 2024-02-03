2b5a622c9aaa2f8efa5d40fe2a975755a4bd7e7e
Linus Rengbrandt <line.rengbrandt@gmail.com>
2024-02-03 19:17:30 +0100

    designed project container and fixed minor fault with about styling

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,5 +1,7 @@
 import "../styling/home-con.css";
 import "../styling/about-con.css";
+import "../styling/projects-con.css";
+
 import { Link } from "react-router-dom";
 
 function LandingPage() {
@@ -40,11 +42,112 @@ function LandingPage() {
       </div>
 
       <div className="projects-container">
-        <h1>Projects</h1>
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Provident
-          vel dolorum exercitationem in quaerat nemo
-        </p>
+        <h1>
+          <span className="highlight">/</span> Projects
+        </h1>
+        <h2>
+          Here are some of my projects that I've worked on. <br /> Feel free to
+          take a look and see what I've been up to.
+        </h2>
+
+        <div className="project">
+          <div className="project-box left">
+            <Link to="/">
+              <h2>Project 1</h2>
+            </Link>
+          </div>
+          <p className="description left">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+            voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+            fugiat iusto fuga praesentium optio, eaque rerum! Provident
+            similique accusantium nemo autem. Veritatis obcaecati tenetur iure
+            eius earum ut molestias architecto voluptate aliquam nihil, eveniet
+            aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur
+            error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+            quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias
+            eos sapiente officiis modi at sunt excepturi expedita sint? Sed
+            quibusdam recusandae alias error harum maxime adipisci amet laborum.
+            Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates
+            a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius
+            fugit doloribus tenetur fugiat, temporibus enim commodi iusto libero
+            magni deleniti quod quam consequuntur! Commodi minima excepturi
+            repudiandae velit hic maxime doloremque. Quaerat provident commodi
+            consectetur veniam similique ad earum omnis ipsum saepe, voluptas,
+            hic voluptates pariatur est explicabo fugiat, dolorum eligendi quam
+            cupiditate excepturi mollitia maiores labore suscipit quas? Nulla,
+            placeat. Voluptatem quaerat non architecto ab laudantium modi minima
+            sunt esse temporibus sint culpa, recusandae aliquam numquam totam
+            ratione voluptas quod exercitationem fuga. Possimus quis earum
+            veniam quasi aliquam eligendi, placeat qui corporis!
+          </p>
+        </div>
+
+        <div className="project">
+          <p className="description right">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+            voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+            fugiat iusto fuga praesentium optio, eaque rerum! Provident
+            similique accusantium nemo autem. Veritatis obcaecati tenetur iure
+            eius earum ut molestias architecto voluptate aliquam nihil, eveniet
+            aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur
+            error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+            quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias
+            eos sapiente officiis modi at sunt excepturi expedita sint? Sed
+            quibusdam recusandae alias error harum maxime adipisci amet laborum.
+            Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates
+            a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius
+            fugit doloribus tenetur fugiat, temporibus enim commodi iusto libero
+            magni deleniti quod quam consequuntur! Commodi minima excepturi
+            repudiandae velit hic maxime doloremque. Quaerat provident commodi
+            consectetur veniam similique ad earum omnis ipsum saepe, voluptas,
+            hic voluptates pariatur est explicabo fugiat, dolorum eligendi quam
+            cupiditate excepturi mollitia maiores labore suscipit quas? Nulla,
+            placeat. Voluptatem quaerat non architecto ab laudantium modi minima
+            sunt esse temporibus sint culpa, recusandae aliquam numquam totam
+            ratione voluptas quod exercitationem fuga. Possimus quis earum
+            veniam quasi aliquam eligendi, placeat qui corporis!
+          </p>
+          <div className="project-box right">
+            <Link to="/">
+              <h2>Project 2</h2>
+            </Link>
+          </div>
+        </div>
+
+        <div className="project">
+          <div className="project-box left">
+            <Link to="/">
+              <h2>Project 3</h2>
+            </Link>
+          </div>
+          <p className="description left">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+            mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+            voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+            fugiat iusto fuga praesentium optio, eaque rerum! Provident
+            similique accusantium nemo autem. Veritatis obcaecati tenetur iure
+            eius earum ut molestias architecto voluptate aliquam nihil, eveniet
+            aliquid culpa officia aut! Impedit sit sunt quaerat, odit, tenetur
+            error, harum nesciunt ipsum debitis quas aliquid. Reprehenderit,
+            quia. Quo neque error repudiandae fuga? Ipsa laudantium molestias
+            eos sapiente officiis modi at sunt excepturi expedita sint? Sed
+            quibusdam recusandae alias error harum maxime adipisci amet laborum.
+            Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates
+            a cumque velit quibusdam sed amet tempora. Sit laborum ab, eius
+            fugit doloribus tenetur fugiat, temporibus enim commodi iusto libero
+            magni deleniti quod quam consequuntur! Commodi minima excepturi
+            repudiandae velit hic maxime doloremque. Quaerat provident commodi
+            consectetur veniam similique ad earum omnis ipsum saepe, voluptas,
+            hic voluptates pariatur est explicabo fugiat, dolorum eligendi quam
+            cupiditate excepturi mollitia maiores labore suscipit quas? Nulla,
+            placeat. Voluptatem quaerat non architecto ab laudantium modi minima
+            sunt esse temporibus sint culpa, recusandae aliquam numquam totam
+            ratione voluptas quod exercitationem fuga. Possimus quis earum
+            veniam quasi aliquam eligendi, placeat qui corporis!
+          </p>
+        </div>
       </div>
 
       <div className="contact-container">
