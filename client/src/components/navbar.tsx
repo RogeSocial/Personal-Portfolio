d6b366d721802e5fcd03df5efedfe3ab6bb63e83
Linus Rengbrandt <line.rengbrandt@gmail.com>
2024-02-08 14:36:27 +0100

    added home link in nav

--- client/src/components/navbar.tsx
+++ client/src/components/navbar.tsx
@@ -6,6 +6,7 @@ function navbar () {
         <div className="navbar">
             <Link to="/" className="nav-title"><h1>Linus Rengbrandt</h1></Link>
             <div className="nav-right">
+                <Link to="/">Home</Link>
                 <Link to="/about">About</Link>
                 <Link to="/">Projects</Link>
                 <Link to="/contact">Contact</Link>
